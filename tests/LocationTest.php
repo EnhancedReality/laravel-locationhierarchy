cecbde637defd1f63d5910eabd8096051be55b32
Ivan Sobtchouk <ivan.sobtchouk@outlook.com>
2018-12-14 19:28:37 -0500

    LocationTest: add one more municipality

--- tests/LocationTest.php
+++ tests/LocationTest.php
@@ -12,16 +12,17 @@ class LocationTest extends TestCase
 {
     public function test_it_can_create_locations()
     {
-        $shire = Region::create(['name'=>'The Shire']);
+        $westfarthing = Region::create(['name'=>'West Farthing']);
         $this->assertCount(1,Region::all());
 
-        $hobbitton = Municipality::createIn($shire,'Hobbitton');
-        $this->assertCount(1,Municipality::all());
+        $hobbitton = Municipality::createIn($westfarthing,'Hobbitton');
+        $bywater = Municipality::createIn($westfarthing,'Bywater');
+        $this->assertCount(2,Municipality::all());
 
         $underhill = Community::createIn($hobbitton,'Underhill');
         $this->assertCount(1,Community::all());
         
-        $this->assertEquals($hobbitton->id,$shire->children()->first()->id);
+        $this->assertEquals($hobbitton->id,$westfarthing->children()->first()->id);
         $this->assertEquals($hobbitton->id,$underhill->parent()->id);
     }
 }
